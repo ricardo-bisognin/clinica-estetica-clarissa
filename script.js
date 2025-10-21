68d7134f6eebd6e391274a3b9fa8b6b2739d95e7
ricardo-dias <bisognin.dias@gmail.com>
2025-10-21 18:49:30 -0300

    Incluída seção "depoimento de clientes", ainda pendente e conclusão (swiper)

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+
 /*Atualiza o tempo de experiência*/
 function calcularTempoExperiencia (anoInicial) {
     const anoAtual = new Date().getFullYear();
@@ -11,3 +13,40 @@ if (elementoAnos) {
 
 /*Atualiza o ano no rodapé*/
 document.getElementById("current-year").textContent = new Date().getFullYear();
+
+/*Inclui o swiper para o depoimento de clientes*/
+const swiper = new Swiper('.testimonial-swiper-container', {
+    /*Opções do swiper*/
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+
+    /*Paginação*/
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    /*Botões de navegação*/
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    /*Responsividade - break points*/
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            centeredSlides: false,
+        },
+
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+            centeredSlides: false,
+        },
+    }
+});
+});
